f6fd77c84332f45a2ba7d3e93cc087cf532bf537
Abunasyr <abunasyr1@gmail.com>
2026-06-01 12:00:03 +0500

    feat: add new function ActionInfo

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,9 +1,14 @@
 package trainings
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
 )
 
 type Training struct {
@@ -14,9 +19,63 @@ type Training struct {
 }
 
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	words := strings.Split(datastring, ",")
+
+	if len(words) != 3 {
+		return errors.New("the data is not full")
+	}
+
+	steps, err := strconv.Atoi(words[0])
+
+	if err != nil {
+		return errors.New("something went wrong")
+	}
+
+	if steps <= 0 {
+		return errors.New("steps must be greater than 0")
+	}
+
+	t.Steps = steps
+
+	t.TrainingType  = words[1]
+
+	parsedTime, err := time.ParseDuration(words[2])
+
+	if err != nil {
+		return errors.New("something went wrong")
+	}
+
+	if parsedTime <= 0 {
+		return errors.New("duration must be greater than 0")
+	}
+
+	t.Duration = parsedTime
+
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	averageSpeed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	var calories float64
+	var err error
+
+	switch t.TrainingType {
+	case "Ходьба":
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	case "Бег":
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+		t.TrainingType, t.Duration.Hours(), distance, averageSpeed, calories,
+		), nil
 }
